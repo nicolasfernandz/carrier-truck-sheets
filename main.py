c7d16e86cf3cca39243b516b0f0fdd1aba81b731
Nicolas <nicojfernandez@gmail.com>
2025-01-31 09:55:19 -0300

    Run black format to new python files

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ from json_minify import json_minify
 
 from carrierfreight import CarrierFreight
 
-#Aplication logger
-log_filename = 'log_' + '.log'
+# Aplication logger
+log_filename = "log_" + ".log"
 logger = logging.getLogger()
-logging.basicConfig(filename=log_filename,level=logging.DEBUG)
+logging.basicConfig(filename=log_filename, level=logging.DEBUG)
+
 
 def main():
-    """ Main script to run the Program"""
+    """Main script to run the Program"""
     # current directory
     directory = os.path.dirname(os.path.abspath(__file__))
     configuration_path = getProperties()
@@ -26,26 +27,32 @@ def main():
         main_graph = json.loads(json_minify(json_string))
         main_graph_str = convert_unicode_dict(main_graph)
 
-        logger.info("the configuration properties path has been read correctly: {}".format(configuration_path))
+        logger.info(
+            "the configuration properties path has been read correctly: {}".format(
+                configuration_path
+            )
+        )
 
         input_paths = main_graph_str["input"]
         type = main_graph_str["type"]
 
-        if(type == "carrierfreight"):
+        if type == "carrierfreight":
             CarrierFreight(main_graph_str=main_graph_str, input_paths=input_paths).run()
-        elif(type == 'control'):
-            print('Not implemented yet.')
+        elif type == "control":
+            print("Not implemented yet.")
 
         logger.info("The process has been completed")
 
+
 def getProperties():
-    """ read from file.properties the main files path"""
-    file = open('file.properties')
+    """read from file.properties the main files path"""
+    file = open("file.properties")
     lines = file.readlines()
     return lines[0]
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     try:
         main()
     except Exception as e:
-        logger.error('Failed to execute program: {}'.format(str(e), exc_info=True))
+        logger.error("Failed to execute program: {}".format(str(e), exc_info=True))
